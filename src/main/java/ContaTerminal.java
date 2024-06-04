f1364b024664acb27f3d07cb779fdf6632461522
João Vitor Fonseca da Silva <144865696+vitorfscs@users.noreply.github.com>
2024-06-04 12:01:39 -0300

    Add files via upload

---
+++ src/main/java/ContaTerminal.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ContaTerminal{
+    public static void ContaTerminal(String[] args) {
+        //Conhecer e importar classes
+        int contaNum = 1021;
+        String agencia = "067-8";
+        String nomeCliente = "MARIO ANDRADE";
+        double saldo = 237.48;
+        //Exibir mensagens para os usuarios
+        System.out.println("Escolha Qual Opção para visualizar: 1 Conta. 2 Agencia. 3 nome. 4 Saldo ");
+        int Opcoes = scanner.nextInt();
+        
+        //Obter valores e scanners para o terminal
+        
+        if(Opcoes == 1){
+    System.out.println("Sua Conta é " + ContaNum);
+        }
+        
+        if(Opcoes == 2){
+    System.out.println("Sua Agência é " + agencia);
+        }
+        
+        
+        if(Opcoes == 3){
+    System.out.println("Seu nome é " + nomeCliente);
+        }
+        
+        
+        if(Opcoes == 4){
+    System.out.println("Seu Saldo é " + saldo);
+        }
+        //exibir conta criada
+    }
+}
